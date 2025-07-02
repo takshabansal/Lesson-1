b13ee5591b311cd0a297236e966f1eaa6a8d8502
takshabansal <takshabansal@gmail.com>
2025-07-02 17:46:04 +0530

    LESSON 1 activities

---
+++ Activityone.py
@@ -0,0 +1,10 @@
+#printing greetings
+print("Hello World from Taksha")
+
+#printing my name
+print("My name is Taksha")
+
+print("I love to build with LEGO")#printing a fun fact about myself
+
+#perfoming and printing a simple calculation
+print("The result of number 3+5 is",3+5) 
